ce4665b337cc3a5156832b1ab124767331dba582
Mark McDonnell <mark.mcdx@gmail.com>
2012-01-15 15:04:23 +0000

    Add Array methods

---
+++ Assets/Scripts/Utils/Array/indexOf.js
@@ -0,0 +1,26 @@
+define(function () {
+
+    /**
+     * ES5 Array.indexOf
+     * @author Miller Medeiros
+     * @version 0.2.0 (2011/11/15)
+     */
+	var indexOf = Array.prototype.indexOf?
+                    function (arr, item, fromIndex) {
+                        return arr.indexOf(item, fromIndex);
+                    } :
+                    function (arr, item, fromIndex) {
+                        fromIndex = fromIndex || 0;
+                        var n = arr.length,
+                            i = fromIndex < 0? n + fromIndex : fromIndex;
+                        for (; i < n; i++) {
+                            if (arr[i] === item) {
+                                return i;
+                            }
+                        }
+                        return -1;
+                    };
+
+    return indexOf;
+    
+});
---
+++ Assets/Scripts/Utils/array.js
@@ -0,0 +1,10 @@
+define(function(require){
+
+	return {
+		filter: require('Utils/Array/filter'),
+		forEach: require('Utils/Array/forEach'),
+		indexOf: require('Utils/Array/indexOf'),
+		unique: require('Utils/Array/unique')
+	};
+
+});
